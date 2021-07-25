7a177ec5ac18e9e5fc51b17675da12964f612257
Shahriyor <shahriyordeveloping@gmail.com>
2021-07-25 12:26:18 +0300

    initial

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
 function dropdown(val) {
-  var y = document.getElementsByClassName('dropdown-toggle');
+  var y = document.getElementsByClassName('header__dropdown__button1 dropdown-toggle');
   var aNode = y[0].innerHTML = val + ' <span class="caret"></span>'; // Append 
 }
+
+function dropdown2(val){
+  var y = document.getElementsByClassName('header__dropdown__button2 dropdown-toggle');
+  var aNode = y[0].innerHTML=val;
+  }
