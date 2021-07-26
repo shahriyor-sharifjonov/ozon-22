9b73eefbb31f6be419ed3f20fd4fec8cfc9257bb
Shahriyor <shahriyordeveloping@gmail.com>
2021-07-27 01:47:18 +0300

    initial

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,105 @@
 function dropdown(val) {
-  var y = document.getElementsByClassName('header__dropdown__button1 dropdown-toggle');
+  var y = document.getElementsByClassName('header__dropdown__button1');
   var aNode = y[0].innerHTML = val + ' <span class="caret"></span>'; // Append 
 }
 
 function dropdown2(val){
-  var y = document.getElementsByClassName('header__dropdown__button2 dropdown-toggle');
+  var y = document.getElementsByClassName('header__dropdown__button2');
   var aNode = y[0].innerHTML=val;
-  }
+}
+function dropdown3(val){
+  var y = document.getElementsByClassName('header__dropdown__button3');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown4(val){
+  var y = document.getElementsByClassName('header__dropdown__button4');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown5(val){
+  var y = document.getElementsByClassName('header__dropdown__button5');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown6(val){
+  var y = document.getElementsByClassName('header__dropdown__button6');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown7(val){
+  var y = document.getElementsByClassName('header__dropdown__button7');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown8(val){
+  var y = document.getElementsByClassName('header__dropdown__button8');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown9(val){
+  var y = document.getElementsByClassName('header__dropdown__button9');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown10(val){
+  var y = document.getElementsByClassName('header__dropdown__button10');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown11(val){
+  var y = document.getElementsByClassName('header__dropdown__button11');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown12(val){
+  var y = document.getElementsByClassName('header__dropdown__button12');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown13(val){
+  var y = document.getElementsByClassName('header__dropdown__button13');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown14(val){
+  var y = document.getElementsByClassName('header__dropdown__button14');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown15(val){
+  var y = document.getElementsByClassName('header__dropdown__button15');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown16(val){
+  var y = document.getElementsByClassName('header__dropdown__button16');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown17(val){
+  var y = document.getElementsByClassName('header__dropdown__button17');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown18(val){
+  var y = document.getElementsByClassName('header__dropdown__button18');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown19(val){
+  var y = document.getElementsByClassName('header__dropdown__button19');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown20(val){
+  var y = document.getElementsByClassName('header__dropdown__button20');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown21(val){
+  var y = document.getElementsByClassName('header__dropdown__button21');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown22(val){
+  var y = document.getElementsByClassName('header__dropdown__button22');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown23(val){
+  var y = document.getElementsByClassName('header__dropdown__button23');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown24(val){
+  var y = document.getElementsByClassName('header__dropdown__button24');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown25(val){
+  var y = document.getElementsByClassName('header__dropdown__button25');
+  var aNode = y[0].innerHTML=val;
+}
+function dropdown26(val){
+  var y = document.getElementsByClassName('header__dropdown__button26');
+  var aNode = y[0].innerHTML=val;
+}
